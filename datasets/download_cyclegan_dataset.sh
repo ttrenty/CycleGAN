632b43033b3037438538c8a1a9e42accb8691245
ttrenty <154608953+ttrenty@users.noreply.github.com>
2024-05-05 10:51:11 +0200

    feat: add male2female64 dataset to cycleGAN

--- datasets/download_cyclegan_dataset.sh
+++ datasets/download_cyclegan_dataset.sh
@@ -1,10 +1,24 @@
-if [ -d "./datasets/apple2orange64" ]; then
+
+FILE=$1
+
+if [[ $FILE != "apple2orange64" && $FILE != "male2female64" ]]; then
+    echo "Available datasets are: apple2orange64, male2female64"
+    exit 1
+fi
+
+if [ -d "./datasets/$FILE" ]; then
     echo "Dataset already exists. If you want to redownload, please delete the existing dataset."
     exit 0
 fi
 
-FILE=apple2orange
-URL="https://drive.google.com/uc?export=download&id=1GSpQs_9hqCcXimYfVcbNDnkJ6K09Sywj" # apple2orange 64x64
+if [ $FILE == "apple2orange64" ]; then
+    URL="https://drive.google.com/uc?export=download&id=1GSpQs_9hqCcXimYfVcbNDnkJ6K09Sywj"
+fi
+
+if [ $FILE == "male2female64" ]; then
+    URL="https://drive.google.com/uc?export=download&id=15-FeMAd1nKEtga2GxcjgtTmD2lV0IepN"
+fi
+
 ZIP_FILE=./datasets/$FILE.zip
 TARGET_DIR=./datasets/$FILE/
 wget -N $URL -O $ZIP_FILE
